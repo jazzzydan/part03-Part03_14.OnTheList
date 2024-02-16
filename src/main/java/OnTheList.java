3f3bb8bd822bd13951be1524e4e94a02225c3ffd
Daniil Starov <jazzzydan@hotmail.com>
2024-02-16 18:58:31 +0200

    Initial commit

---
+++ src/main/java/OnTheList.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OnTheList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+        System.out.println("");
+        System.out.print("Search for? ");
+        String searchFor = scanner.nextLine();
+
+        if (list.contains(searchFor)) {
+            System.out.println(searchFor + " was found!");
+        } else {
+            System.out.println(searchFor + " was not found!");
+        }
+    }
+}
